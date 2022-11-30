a1e59a298ae17d3e51b9bde9dba0661f40c39480
Cristiano Marques Silva <cristimarsilva.cris@gmail.com>
2022-11-30 10:28:57 -0300

    revisão de Programação web
    consegui desenvolver o projeto, utilizando todos os requisitos pedidos.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var país = prompt("ME DIZ, PARA QUE PAÍS VOCÊ GOSTARIA DE IR: ");
+
+alert ('Seja Bem vindo, iremos fazer o possivel para que você consiga ir para o(a)' +país+ ', complete o formulário para que possamos prosseguir.')
+
+function enviar(){
+alert("Obrigado por completar o cadastro.")
+}
